6648e03fba1d236f4592b52baf4b15a314d4d074
Ben Burdette <>
2023-04-15 15:16:03 -0600

    docs, cleanup

--- src/HttpJsonTask.elm
+++ src/HttpJsonTask.elm
@@ -1,11 +1,30 @@
-module HttpJsonTask exposing (getJsonTask, jsonResolver, jsonTask, postJsonTask, resolve)
+module HttpJsonTask exposing
+    ( jsonResolver
+    , JsonArgs
+    , jsonTask
+    , postJsonTask
+    , getJsonTask
+    , resolve
+    )
+
+{-| Some basic json utils for use with elm/http.
+
+@docs jsonResolver
+@docs JsonArgs
+@docs jsonTask
+@docs postJsonTask
+@docs getJsonTask
+@docs resolve
+
+-}
 
 import Http exposing (Error(..), Resolver, Response(..))
 import Json.Decode as JD
-import Json.Encode as JE
 import Task exposing (Task)
 
 
+{-| jsonResolver, similar to stringResolver or bytesResolver in elm/http.
+-}
 jsonResolver : JD.Decoder a -> Resolver Error a
 jsonResolver decoder =
     Http.stringResolver <|
@@ -15,20 +34,14 @@ jsonResolver decoder =
             )
 
 
-postJsonTask : JsonArgs a -> Task Error a
-postJsonTask args =
-    jsonTask "POST" args
-
-
-getJsonTask : JsonArgs a -> Task Error a
-getJsonTask args =
-    jsonTask "GET" args
-
-
+{-| Args struct for jsonTask and its relatives.
+-}
 type alias JsonArgs a =
     { url : String, body : Http.Body, decoder : JD.Decoder a }
 
 
+{-| make a json task; specify the http method as a string.
+-}
 jsonTask : String -> JsonArgs a -> Task Error a
 jsonTask method args =
     Http.task
@@ -41,6 +54,22 @@ jsonTask method args =
         }
 
 
+{-| jsonTask with method POST.
+-}
+postJsonTask : JsonArgs a -> Task Error a
+postJsonTask args =
+    jsonTask "POST" args
+
+
+{-| jsonTask with method GET.
+-}
+getJsonTask : JsonArgs a -> Task Error a
+getJsonTask args =
+    jsonTask "GET" args
+
+
+{-| lifted out of elm/http
+-}
 resolve : (body -> Result String a) -> Response body -> Result Error a
 resolve toResult response =
     case response of
